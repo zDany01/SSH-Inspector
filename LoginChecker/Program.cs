28c21f28efcf931d3c2a84abdd622bbc20f9a192
zDany01 <85403430+zDany01@users.noreply.github.com>
2024-04-24 22:16:50 +0200

    Finished Project

---
+++ LoginChecker/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LoginChecker
+{
+    internal class Program
+    {
+        public static List<(string, int)> CountryCount = new List<(string, int)>();
+
+        private static void SortList()
+        {
+            List<(string, int)> OrderedList = new List<(string, int)>();
+            do
+            {
+                (string, int) biggestCountry = ("", 0);
+                foreach (var country in CountryCount) if (country.Item2 > biggestCountry.Item2) biggestCountry = country;
+                OrderedList.Add(biggestCountry);
+                CountryCount.Remove(biggestCountry);
+            } while(CountryCount.Count > 0);
+            CountryCount = OrderedList;
+        }
+
+        public static void CheckCountry(string country)
+        {
+            bool found = false;
+            for (int i = 0; i < CountryCount.Count; i++)
+            {
+                (string, int) svdcountry = CountryCount[i];
+                if (svdcountry.Item1 == country)
+                {
+                    svdcountry.Item2++;
+                    found = true;
+                    CountryCount[i] = svdcountry;
+                    break;
+                }
+            }
+            if (!found) CountryCount.Add((country, 1));
+        }
+        static void Main(string[] args)
+        {
+            string[] fileContent;
+            try
+            {
+                fileContent = File.ReadAllLines(args[0]);
+            }
+            catch
+            {
+                Console.WriteLine("Usage:\nLoginChecker <pathToLogFile>");
+                return;
+            }
+
+            Console.WriteLine("Checking...");
+            List<Scammer> scammers = new List<Scammer>();
+            foreach (string line in fileContent)
+            {
+                string ip = FilterData(line, out string user);
+                bool found = false;
+
+                foreach (Scammer scammer in scammers)
+                {
+                    if (scammer.IP == ip)
+                    {
+                        scammer.RegisterUsername(user);
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    Scammer bastard = new Scammer(ip);
+                    bastard.RegisterUsername(user);
+                    scammers.Add(bastard);
+                }
+            }
+
+            Console.WriteLine("Check done, press enter to show result...");
+            Console.ReadLine();
+            foreach (Scammer pieceOfShit in scammers) pieceOfShit.TableOfShame();
+
+            Console.WriteLine("Done.");
+            Console.WriteLine("I'm most wanted from:");
+            SortList();
+            foreach ((string, int) country in CountryCount) Console.WriteLine($"{country.Item1} - {country.Item2}");
+
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+        }
+
+        private static string FilterData(string line, out string userName)
+        {
+            int userStart = line.IndexOf("for") + 4; //for + space
+            int userEnd = line.IndexOf("from") - 1;
+            int ipStart = userEnd + 6; //from + space + -1
+            int ipEnd = IndexOfWholeWord(line, "port") - 1;
+            userName = line.Substring(userStart, userEnd - userStart);
+            return line.Substring(ipStart, ipEnd - ipStart);
+        }
+
+        static int IndexOfWholeWord(string str, string word)
+        {
+            for (int j = 0; j < str.Length &&
+                (j = str.IndexOf(word, j, StringComparison.Ordinal)) >= 0; j++)
+                if ((j == 0 || !char.IsLetterOrDigit(str, j - 1)) &&
+                    (j + word.Length == str.Length || !char.IsLetterOrDigit(str, j + word.Length)))
+                    return j;
+            return -1;
+        }
+    }
+}
---
+++ LoginChecker/Scammer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Net;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LoginChecker
+{
+    internal class Scammer
+    {
+        public string IP { get; private set; }
+        public List<string> UserNames { get; private set; }
+        private int loginAttempt = 0;
+
+        public Scammer(string IP)
+        {
+            this.IP = IP;
+            UserNames = new List<string>();
+        }
+
+        public void RegisterUsername(string username)
+        {
+            if(!UserNames.Contains(username)) UserNames.Add(username);
+            loginAttempt++;
+        }
+
+
+        private void WriteTableLine(string line, int fullSize, char lastChar = '\n')
+        {
+            string formattedString = $"| {line}";
+            Console.Write(formattedString);
+            int noSpace = fullSize - formattedString.Length - 1;
+            for (int i = 0; i < noSpace; i++) Console.Write(' ');
+            Console.Write($"|{lastChar}");
+        }
+
+        string GetJValue(JObject json, string valueName)
+        {
+            int tries = 3;
+            do
+            try
+            {
+                return json[valueName].Value<string>();
+            } catch
+            {
+                    System.Threading.Thread.Sleep(1500);
+            } while (tries-- > 0);
+            return "Error";
+
+        }
+
+        public void TableOfShame()
+        {
+            if (IP.StartsWith("192.168")) return;
+            JObject apiResponse = JObject.Parse(new WebClient().DownloadString($"http://ipwho.is/{IP}"));
+
+            string country = GetJValue(apiResponse, "country");
+            Program.CheckCountry(country);
+            for (int i = 0; i < UserNames.Count; i++) if (UserNames[i].StartsWith("invalid user")) UserNames[i] = UserNames[i].Replace("invalid user", null);
+
+            string header = $"/----------------------{IP}----------------------\\";
+            int fullSize = header.Length;
+            Console.WriteLine(header);
+            WriteTableLine($"IP Geolocation: {GetJValue(apiResponse, "continent")} - {country}({GetJValue(apiResponse, "country_code")})", fullSize);
+            WriteTableLine($"Region/State: {GetJValue(apiResponse, "region")}", fullSize);
+            WriteTableLine($"City: {GetJValue(apiResponse, "city")}", fullSize);
+            WriteTableLine($"Login Attempt: {loginAttempt}", fullSize);
+            WriteTableLine("UserNames:", fullSize);
+
+            string userNameLine = string.Empty;
+            foreach(string userName in UserNames)
+            {
+                string newString = userNameLine + userName + ", ";
+                if (newString.Length >= fullSize - 2)
+                {
+                    WriteTableLine(userNameLine, fullSize);
+                    userNameLine = string.Empty;
+                }
+                else userNameLine = newString;
+
+            }
+            WriteTableLine(userNameLine + "\b\b ", fullSize + 4);
+            Console.Write("\\"); //delete the last comma
+            for (int i = 0; i < fullSize - 2; i++) Console.Write('-');
+            Console.WriteLine("/");
+        }
+
+    }
+}
